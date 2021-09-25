f8fd369bd234912ce44712f0e05d32301706f765
BengalDroid <bengaldroid@gmail.com>
2021-09-25 11:00:55 +0600

    factorial

---
+++ factorial.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n, i, fact = 1;
+  cout << "Enter a number : ";
+  cin >> n;
+  for (i = 1; i <= n; i++)
+  {
+    fact *= i;
+  }
+  cout << "factorial of " << n << " is : " << fact << endl;
+  return 0;
+}
